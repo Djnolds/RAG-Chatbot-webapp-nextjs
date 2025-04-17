15a06120d9384060daf536713b55136066e93110
Djnolds <ayoreynolds@gmail..com>
2025-04-17 16:04:36 +0100

    second commit

--- lib/langchain.ts
+++ lib/langchain.ts
@@ -1,7 +1,7 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { StringOutputParser } from "@langchain/core/output_parsers";
-import { RunnableSequence } from "@langchain/core/runnables";
+// import { RunnableSequence } from "@langchain/core/runnables";
 import { VectorStore } from "@langchain/core/vectorstores";
 
 interface ProcessMessageArgs {
